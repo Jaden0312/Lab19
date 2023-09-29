ef5392b277e9799abb1eb413145a4b17fc9f6e7d
Jaden0312 <eve0knight88@gmai.com>
2023-09-29 16:28:35 +0900

    v2

--- lab19.js
+++ lab19.js
@@ -1,26 +1,43 @@
 const myApiNum = 'ZH6E0owuhMP-3aeuf6bwoJb8kQyFIh97S221WDIoeSI';
-const url = 'https://api.unsplash.com/search/photos?page=${page}&query='
+let page = 1 
+const url = `https://api.unsplash.com/search/photos?page=${page}&query=`;
 const searchIcon = document.getElementById('search-btn');
 const searchBar = document.getElementById('search-box');
 const searchResult = document.getElementById('search-result');
 let searchValue = searchBar.value;
 
 
-async function searchImage(searchValue) {
-    const response = await fetch(url + searchValue + '&client_id='+ myApiNum);
-    let jsonData = await response.json();
+async function searchImage() {
+    const response = await fetch(url + searchBar.value + '&client_id='+ myApiNum);
+    const jsonData = await response.json();
     console.log(jsonData);
     // for (i=0 ; i <= length-1 ; i++) {
     //     const imageElements = document.createElement('img');
     //     imageElements.src = jsonData.results[i].urls.small;
     //     searchResult.appendChild(imageElements);
+    //     console.log('for loop',searchResult);
     // };
-    const imageElements = document.createElement('img');
-    imageElements.src = jsonData.results[0].urls.small;
-    searchResult.appendChild(imageElements);
+    // console.log('outside',searchResult.children);
+
+    jsonData.results.map((result) => {
+        console.log(result);
+        const imageElements = document.createElement('img');
+        imageElements.src = result.urls.small;
+        console.log('before',searchResult);
+        searchResult.appendChild(imageElements);
+        console.log('after',searchResult);
+    });
+
+    // const imageElements = document.createElement('img');
+    // imageElements.src = jsonData.results
+
+    // const imageElements = document.createElement('img');
+    // imageElements.src = jsonData.results[0].urls.small;
+    // searchResult.appendChild(imageElements);
 };
 searchImage();
 
-searchIcon.addEventListener ('click', () => {
-    searchImage(searchValue);
+searchIcon.addEventListener('click', () => {
+    console.log('click');
+    searchImage();
 });
